93cbdec0cbd95b000ed958b73b58f82ceb6b8e2b
Travis <travis.lannoye@gmail.com>
2020-12-22 16:26:55 -0600

    Styled CardDisplay

--- companion-front/src/components/CardDisplay.js
+++ companion-front/src/components/CardDisplay.js
@@ -6,46 +6,49 @@ import Loader from './Loader';
 import Message from './Message';
 
 const CardDisplay = ({ name }) => {
-    const [image, setImage] = useState('');
-    const [uri, setUri] = useState('');
+	const [image, setImage] = useState('');
+	const [uri, setUri] = useState('');
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const cardScryfall = useSelector((state) => state.cardScryfall);
-    const { loading, error, cardData } = cardScryfall;
+	const cardScryfall = useSelector((state) => state.cardScryfall);
+	const { loading, error, cardData } = cardScryfall;
 
-    useEffect(() => {
-        if (name) {
-            if (!cardData) {
-                dispatch(getCardScryfall(name));
-            } else {
-                setImage(cardData.image_uris.small);
-                setUri(cardData.scryfall_uri);
-            }
-        }
-    }, [dispatch, name, cardData]);
+	useEffect(() => {
+		if (name) {
+			if (!cardData) {
+				dispatch(getCardScryfall(name));
+			} else {
+				setImage(cardData.image_uris.small);
+				setUri(cardData.scryfall_uri);
+			}
+		}
+	}, [dispatch, name, cardData]);
 
-    return (
-        <>
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <Message variant='danger'>{error}</Message>
-            ) : (
-                // <Card className='my-3 p-3 rounded'>
-                <Card border='white'>
-                    <Card.Img src={image} variant='top' />
-                    <Card.Body>
-                        {/* <Card.Title as='div'>
+	return (
+		<>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				// <Card className='my-3 p-3 rounded'>
+				<Card
+					border='white'
+					className='text-center'
+					style={{ maxWidth: '200px' }}>
+					<Card.Img src={image} variant='top' />
+					<Card.Body>
+						{/* <Card.Title as='div'>
                             <strong>{name}</strong>
                         </Card.Title> */}
-                        {/* <Card.Text as='div'>{edition}</Card.Text> */}
-                        <Card.Link href={uri}>Scryfall</Card.Link>
-                    </Card.Body>
-                </Card>
-            )}
-        </>
-    );
+						{/* <Card.Text as='div'>{edition}</Card.Text> */}
+						<Card.Link href={uri}>Scryfall</Card.Link>
+					</Card.Body>
+				</Card>
+			)}
+		</>
+	);
 };
 
 export default CardDisplay;
